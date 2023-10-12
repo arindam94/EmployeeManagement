f7d054b4bf1498d3ea2db8c29885e82c03c1085b
arindam94 <>
2023-10-12 15:31:10 +0530

    fixed code indentation

--- EmployeeManagmentTests/Feature/EmployeeDetails/EmployeeDetailsCellTests.swift
+++ EmployeeManagmentTests/Feature/EmployeeDetails/EmployeeDetailsCellTests.swift
@@ -10,7 +10,7 @@ import XCTest
 
 final class EmployeeDetailsCellTests: XCTestCase {
     
-    func testEmployeeCell(){
+    func testEmployeeCell() {
         let nib = UINib(nibName: "EmployeeDetailsCell", bundle: Bundle(for: EmployeeDetailsCell.self))
         if let cell = nib.instantiate(withOwner: self, options: nil).first as? EmployeeDetailsCell{
             if let employeeData = MockDataProvider.getEmployeeData()?.users?.first{
